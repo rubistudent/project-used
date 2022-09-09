9d6fd359744579f9346b05337205a7e6e29e3128
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2022-09-10 00:01:50 +0300

    complited

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,18 @@ const baseUrl="https://fakestoreapi.com/products"
 
 document.addEventlistener('DomcontentLoaded',(e)=>{
     e.preventDefault()
-    const fetchproducts=()=>{
+    const fetchproducts=()=>{ 
         fetch(baseUrl)
         .then((response)=>response.jgon ())
         .then((data)=>showProducts(data))
-
+let data1= "  "
+response.map((values ))=>{}
+data1='</div>id="card"
+<h2>class="title">$ { values }></h2>
+ <img src=""alt="images">
+ <p> description</p>
+ <p class="category">category   </p>
+ <p class="price">price</p>  '
     }
         fetchproducts()
 })
